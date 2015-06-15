b6fb5b8c1a74ce538375313c8f0b182b8ddf32fd
Fabian Witt <Fabian@SgtBlack.fritz.box>
2015-06-15 20:24:24 +0200

    Aufruf mit args

--- fuzzyHAC/src/process/Main.java
+++ fuzzyHAC/src/process/Main.java
@@ -7,57 +7,50 @@ public class Main {
 
     /**
      * @param args the command line arguments
+     * args[0] the inputFile
+     * args[1] Anzahl der Cluster
+     * args[2] Linkage Methode
+     * args[3] K -> Anzahl der einbezogenen Datenpunkte beim Linkage
      */
     public static void main(String[] args) {
-        
-        int k = 10;
-        int anz_cluster = 3;
-        
-        
-        // read data file
-        //ReadFile rf = new ReadFile( "iris.arff" );
-        //ReadFile rf = new ReadFile( "easy.arff" );
-        //ReadFile rf = new ReadFile( "triangles.arff" );
 
-        //ReadFile rf = new ReadFile( "iris.arff" );
-        ReadFile rf = new ReadFile( "easy.arff" );
-        //ReadFile rf = new ReadFile( "triangles.arff" );
+        if (args.length == 4) {
 
-        
-        // create object for distance calculation
-        Distance d = new Distance( rf.getData() );
+            int anz_cluster = Integer.parseInt( args[1] );
+            int k = Integer.parseInt( args[3] );
+            
+            
+            // read data file
+            ReadFile rf = new ReadFile( args[0] );
 
-        // get the distance matrix from each data point to each data point
-        double[][] distances = d.getDistMatrix();
-        
-        // get the names of the data points (0...n)
-        String[] names = new String[distances.length];
-        for(int i = 0; i < distances.length; i++){
-            names[i] = Integer.toString(i);
+            // create object for distance calculation
+            Distance d = new Distance( rf.getData() );
+
+            // get the distance matrix from each data point to each data point
+            double[][] distances = d.getDistMatrix();
+
+            // get the names of the data points (0...n)
+            String[] names = new String[distances.length];
+            for(int i = 0; i < distances.length; i++){
+                names[i] = Integer.toString(i);
+            }
+
+            // cluster with specified algorithm
+            ClusteringAlgorithm alg = new DefaultClusteringAlgorithm();
+            
+            LinkageStrategy strategy;
+            
+            switch( args[2] ){
+                case "single": strategy = new SingleLinkageStrategy(); break;
+                case "complete": strategy = new CompleteLinkageStrategy(); break;
+                default: strategy = new SingleLinkageStrategy(); break;
+            }
+
+            Cluster cluster = alg.performClusteringK(distances, names, strategy, k);
+
+            // write a file with the class labels
+            WriteLabelFile labelfile = new WriteLabelFile(cluster, anz_cluster);
+            labelfile.createFile("sol_k_linkage"); 
         }
-        
-        // cluster with specified algorithm
-        ClusteringAlgorithm alg = new DefaultClusteringAlgorithm();
-        
-        Cluster cluster_single = alg.performClustering(distances, names, new SingleLinkageStrategy());
-        Cluster cluster_single_k = alg.performClusteringK(distances, names, new SingleLinkageStrategy(),k);
-        
-        Cluster cluster_complete = alg.performClustering(distances, names, new CompleteLinkageStrategy());
-        Cluster cluster_complete_k = alg.performClusteringK(distances, names, new CompleteLinkageStrategy(),k);
-        
-        
-        // write a file with the class labels
-        WriteLabelFile labelfile_single = new WriteLabelFile(cluster_single,anz_cluster);
-        labelfile_single.createFile("sol_single");
-        
-        WriteLabelFile labelfile_single_k = new WriteLabelFile(cluster_single_k,anz_cluster);
-        labelfile_single_k.createFile("sol_single_k");
-        
-        WriteLabelFile labelfile_complete = new WriteLabelFile(cluster_complete,anz_cluster);
-        labelfile_complete.createFile("sol_complete");
-        
-        WriteLabelFile labelfile_complete_k = new WriteLabelFile(cluster_complete_k,anz_cluster);
-        labelfile_complete_k.createFile("sol_complete_k");
-        
     }
 }
